9761a948b2a242d2fb2d3334fbdc0a89a486e5f4
tengyuchen07-alt <tengyuchen07@gmail.com>
2025-11-08 14:58:41 +0800

    Implement HugeInt structure and arithmetic operations

---
+++ 0704.cpp
@@ -0,0 +1,189 @@
+#include<iostream>
+#include<cstring>
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct HugeInt
+{
+    int size;
+    int* digit;
+};
+
+// quotient = dividend / divisor; remainder = dividend % divisor
+// provided that dividend != 0, divisor != 0 and dividend >= divisor
+void division(HugeInt dividend, HugeInt divisor, HugeInt& quotient, HugeInt& remainder);
+
+// hugeInt /= 10, or equivalently, shifts right by one position
+void divideBy10(HugeInt& hugeInt);
+
+// minuend -= subtrahend
+// provided that minuend != 0, subtrahend != 0 and minuend >= subtrahend
+void subtraction(HugeInt& minuend, HugeInt subtrahend);
+
+// returns true if and only if hugeInt1 < hugeInt2
+// provided that hugeInt1 != 0 and hugeInt2 != 0
+bool less(HugeInt hugeInt1, HugeInt hugeInt2);
+
+// return true if and only if hugeInt1 == hugeInt2
+// provided that hugeInt1 != 0 and hugeInt2 != 0
+bool equal(HugeInt hugeInt1, HugeInt hugeInt2);
+
+// returns true if and only if the specified huge integer is zero
+bool isZero(HugeInt hugeInt);
+
+const int arraySize = 250;
+
+int main()
+{
+    char strA[251], strB[251];
+
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; ++t)
+    {
+        cin >> strA >> strB;
+
+        HugeInt dividend;
+        dividend.size = strlen(strA);
+        dividend.digit = new int[dividend.size]();
+        for (int i = 0; i < dividend.size; ++i)
+            dividend.digit[i] = strA[dividend.size - 1 - i] - '0';
+
+        HugeInt divisor;
+        divisor.size = strlen(strB);
+        divisor.digit = new int[divisor.size]();
+        for (int i = 0; i < divisor.size; ++i)
+            divisor.digit[i] = strB[divisor.size - 1 - i] - '0';
+
+        HugeInt quotient;
+        HugeInt remainder;
+        division(dividend, divisor, quotient, remainder);
+
+        for (int i = quotient.size - 1; i >= 0; i--)
+            cout << quotient.digit[i];
+        cout << endl;
+
+        for (int i = remainder.size - 1; i >= 0; i--)
+            cout << remainder.digit[i];
+        cout << endl;
+
+        delete[] dividend.digit;
+        delete[] divisor.digit;
+        delete[] quotient.digit;
+        delete[] remainder.digit;
+    }
+}
+
+// quotient = dividend / divisor; remainder = dividend % divisor
+// provided that dividend != 0, divisor != 0 and dividend >= divisor
+void division(HugeInt dividend, HugeInt divisor, HugeInt& quotient, HugeInt& remainder)
+{   
+    quotient.size = dividend.size - divisor.size+1;
+    quotient.digit = new int[quotient.size]();
+    
+        for (int i = quotient.size-1; i >= 0; i--) {   //每一位的商
+
+            HugeInt newdivisor;
+            newdivisor.size = i + divisor.size;
+            newdivisor.digit = new int[newdivisor.size]();
+            for (int m = divisor.size - 1; m >= 0; m--)newdivisor.digit[m + i] = divisor.digit[m];
+
+            while (less(newdivisor, dividend)) {       //除數小於被除數就做
+                subtraction(dividend, newdivisor);
+                quotient.digit[i]+=1;
+            }
+
+            delete[] newdivisor.digit;
+        }
+
+        remainder.size = dividend.size;
+        remainder.digit = new int[remainder.size];
+        for (int i = 0; i < remainder.size; i++)
+            remainder.digit[i] = dividend.digit[i];
+
+        if (quotient.size > 1 && quotient.digit[quotient.size - 1] == 0)    //處理12/6=02等情況
+            quotient.size--;
+}
+
+// hugeInt /= 10, or equivalently, shifts right by one position
+void divideBy10(HugeInt& hugeInt)
+{
+    if (hugeInt.size == 1)
+        hugeInt.digit[0] = 0;
+    else
+    {
+        for (int i = 1; i < hugeInt.size; i++)
+            hugeInt.digit[i - 1] = hugeInt.digit[i];
+
+        hugeInt.size--;
+        hugeInt.digit[hugeInt.size] = 0;
+    }
+}
+
+// minuend -= subtrahend
+// provided that minuend != 0, subtrahend != 0 and minuend >= subtrahend
+void subtraction(HugeInt& minuend, HugeInt subtrahend)
+{
+    if (equal(minuend, subtrahend))
+    {
+        minuend.size = 1;
+        minuend.digit[0] = 0;
+        return;
+    }
+    
+    int i = 0, j = 0;
+    while (j < subtrahend.size)
+    {
+        minuend.digit[j] = minuend.digit[j] - subtrahend.digit[j];
+        j++;
+    }
+    while (j < minuend.size)
+    {
+        minuend.digit[j] = minuend.digit[j];
+        j++;
+    }
+    for (i = 0; i < minuend.size - 1; i++)
+    {
+        if (minuend.digit[i] < 0)
+        {
+            minuend.digit[i] += 10;
+            minuend.digit[i + 1]--;
+        }
+    }
+    while (minuend.size > 1 && minuend.digit[minuend.size - 1] == 0)
+        minuend.size--;
+}
+
+// returns true if and only if hugeInt1 < hugeInt2
+// provided that hugeInt1 != 0 and hugeInt2 != 0
+bool less(HugeInt hugeInt1, HugeInt hugeInt2)
+{
+    if (hugeInt1.size < hugeInt2.size)return true;
+    if (hugeInt1.size > hugeInt2.size)return false;
+    for (int i = hugeInt1.size - 1; i >= 0; i--) {
+        if (hugeInt1.digit[i] < hugeInt2.digit[i])return true;
+        if (hugeInt1.digit[i] > hugeInt2.digit[i])return false;
+    }
+    return true;
+}
+
+// return true if and only if hugeInt1 == hugeInt2
+// provided that hugeInt1 != 0 and hugeInt2 != 0
+bool equal(HugeInt hugeInt1, HugeInt hugeInt2)
+{
+    if (hugeInt1.size != hugeInt2.size)
+        return false;
+
+    for (int i = hugeInt1.size - 1; i >= 0; i--)
+        if (hugeInt1.digit[i] != hugeInt2.digit[i])
+            return false;
+
+    return true;
+}
+
+// returns true if and only if the specified huge integer is zero
+bool isZero(HugeInt hugeInt)
+{
+    return hugeInt.size == 1 && hugeInt.digit[0] == 0;
+}
