ce4fb0a89fbdc4405730bc22b7651855ecca9cbd
tengyuchen07-alt <tengyuchen07@gmail.com>
2025-11-24 13:27:45 +0800

    Add binary display and bit summation functions
    Implement functions to display binary representation and sum bits.

---
+++ mid2-2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// prints the binary representation of number,
+// for example, if number is 10, then prints 1010
+void displayBinary(int number);
+
+// returns the sum of all bits of the binary representation of number,
+// or equivalently the number of 1s in the binary representation of number,
+// for example, if number is 10, then returns 2
+int sumBits(int number);
+
+int main()
+{
+    int number;
+    cin >> number;
+    while (number > 0)
+    {
+        cout << "The parity of ";
+        displayBinary(number);
+
+        cout << " is " << sumBits(number) << " (mod 2).\n";
+
+        cin >> number;
+    }
+
+    system("pause");
+}
+
+void displayBinary(int number)
+{
+    if (number > 0)displayBinary(number / 2);
+    else {
+        return;
+    }
+    cout << number % 2;
+
+}
+
+int sumBits(int number)
+{
+    if (number <= 0)return 0;
+    return number % 2 + sumBits(number / 2);
+}
